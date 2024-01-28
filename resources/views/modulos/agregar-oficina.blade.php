4712e81a4643f29470073de6297f6d39863a4231
Percy Condori Yucra <percycondori11y@gmail.com>
2024-01-28 17:03:50 -0500

    .

--- resources/views/modulos/agregar-oficina.blade.php
+++ resources/views/modulos/agregar-oficina.blade.php
@@ -132,7 +132,7 @@
                                     <td class="font-medium text-gray-900 dark:text-black capitalize">{{ $oficina->sede->nombre_sede }}</td>
                                     <td class="font-medium text-gray-900 dark:text-black capitalize">{{ $oficina->piso }}</td>
 
-                                    <td class="px-6 py-16">
+                                    <td class="flex">
                                         <a href="{{ route('agregar-oficina.edit', $oficina->id) }}" class=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                                             Editar
                                         </a>
